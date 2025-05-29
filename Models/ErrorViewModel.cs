8d672e18bd3cb94ef9cd0ad4d3b4dcf3a3baa830
santos ghimire <ghimiresantos85@gmail.com>
2025-05-29 12:26:48 +0545

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace jooneliwebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
